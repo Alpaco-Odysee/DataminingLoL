ccd5c659775d89a2d7953d314e8d3c0da742792b
Alpaco-Odysee <main.altun.can@gmail.com>
2025-11-03 13:03:26 +0100

    adds player history - not working yet

--- cmd/db.go
+++ cmd/db.go
@@ -12,7 +12,6 @@ func openDB() (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return db, nil
 }
 
@@ -28,7 +27,14 @@ func createDB() {
 	CREATE TABLE IF NOT EXISTS Players (
         league_id TEXT NOT NULL,
         puuid TEXT NOT NULL
-    );`
+    );
+	
+	CREATE TABLE IF NOT EXISTS History (
+		puuid TEXT NOT NULL,
+		data BLOB,
+		PRIMARY KEY(puuid)
+	);
+	`
 
 	db.Exec(schema)
 }
@@ -49,4 +55,35 @@ func addPlayer(l LeagueEntry) error {
 	}
 
 	return nil
+}
+
+func getAllPlayerPuuIDs() ([]string, error) {
+	db, err := openDB()
+	if err != nil {
+		fmt.Println("error:", err)
+		return nil, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT puuid FROM Players")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var puuIDs []string
+
+	for rows.Next() {
+		var puu string
+		err = rows.Scan(&puu)
+		if err != nil {
+			return nil, err
+		}
+		puuIDs = append(puuIDs, puu)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return puuIDs, nil
 }
--- cmd/main.go
+++ cmd/main.go
@@ -8,25 +8,36 @@ import (
 	"time"
 )
 
-const apiKey = "RGAPI-a83dbc55-39d2-4521-96f7-3ead65ea4d3d"
-const baseURL = "https://euw1.api.riotgames.com"
+const (
+	apiKey         = "RGAPI-36752775-a120-4542-a136-0410d920e962"
+	playerBaseURL  = "https://euw1.api.riotgames.com"
+	historyBaseURL = "https://europe.api.riotgames.com"
+)
+
+var start = time.Date(2025, 10, 27, 00, 00, 00, 1, time.UTC).Unix()
+var end = time.Date(2025, 11, 02, 23, 59, 59, 1, time.UTC).Unix()
 
 type LeagueEntry struct {
 	LeagueID string
 	PuuID    string
 }
 
+type PlayerHistory struct {
+	MatchIDs []string
+}
+
 func main() {
+
 	createDB()
 
-	err := handleGetPlayersInRankDivision()
-	if err != nil {
-		fmt.Println(err)
-	}
-	// err = handleGetMatchesFromPlayers()
+	// err := handleGetPlayersInRankDivision()
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
+	err := handleGetMatchesFromPlayers()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func handleGetPlayersInRankDivision() error {
@@ -68,26 +79,16 @@ func handleGetPlayersInRankDivision() error {
 	return nil
 }
 
-// func handleGetMatchesFromPlayers() error {
-// 	puuIDs, err := getAllPlayerPuuIDs()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
 func getLeagueEntries(pageNumber int) ([]LeagueEntry, error) {
 	queue := "RANKED_SOLO_5x5"
 	tier := "DIAMOND"
 	division := "I"
-	url := fmt.Sprintf("%s/lol/league/v4/entries/%s/%s/%s?page=%d", baseURL, queue, tier, division, pageNumber)
+	url := fmt.Sprintf("%s/lol/league/v4/entries/%s/%s/%s?page=%d", playerBaseURL, queue, tier, division, pageNumber)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-
 	req.Header.Set("X-Riot-Token", apiKey)
 
 	client := &http.Client{}
@@ -109,4 +110,61 @@ func getLeagueEntries(pageNumber int) ([]LeagueEntry, error) {
 	}
 
 	return entries, nil
+}
+
+func handleGetMatchesFromPlayers() error {
+	puuIDs, err := getAllPlayerPuuIDs()
+	if err != nil {
+		return err
+	}
+
+	// fmt.Println(puuIDs)
+	position := 0
+	totalCalls := 0
+	for {
+		if totalCalls%20 == 0 {
+			fmt.Println("waiting 2 seconds")
+			time.Sleep(2 * time.Second)
+		}
+		if totalCalls == 100 {
+			fmt.Println("waiting 241 seconds")
+			time.Sleep(241 * time.Second)
+			totalCalls = 0
+		}
+		url := fmt.Sprintf("%s/lol/match/v5/matches/by-puuid/%s/ids?startTime=%d&endTime=%d&start=%d&count=%d", historyBaseURL, puuIDs[position], start, end, 0, 100)
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return err
+		}
+		req.Header.Set("X-Riot-Token", apiKey)
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		var gameHistory []string
+		err = json.NewDecoder(resp.Body).Decode(&gameHistory)
+		if err != nil {
+			return err
+		}
+
+		for _, id := range gameHistory {
+			fmt.Println(id)
+		}
+
+		if len(gameHistory) > 90 {
+			fmt.Println("History länger als 90 gefunden")
+			break
+		} else {
+			fmt.Println(position)
+			position++
+		}
+		totalCalls++
+	}
+
+	return nil
 }
